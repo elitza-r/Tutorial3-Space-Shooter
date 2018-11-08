57967dfa1e0bf83ab8d899b82ab0487951828064
elitza-r <elitza-r@users.noreply.github.com>
2018-11-08 12:55:16 -0500

    Extending the Space Shooter challenge

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -6,6 +6,7 @@ public class DestroyByContact : MonoBehaviour
     public GameObject explosion;
     public GameObject playerExplosion;
     public int scoreValue;
+
     private GameController gameController;
 
     void Start()
@@ -23,16 +24,22 @@ public class DestroyByContact : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Boundary")
+        if (other.CompareTag("Boundary") || other.CompareTag("Enemy"))
         {
             return;
         }
-        Instantiate(explosion, transform.position, transform.rotation);
+
+        if (explosion != null)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+
         if (other.tag == "Player")
         {
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
             gameController.GameOver();
         }
+
         gameController.AddScore(scoreValue);
         Destroy(other.gameObject);
         Destroy(gameObject);
